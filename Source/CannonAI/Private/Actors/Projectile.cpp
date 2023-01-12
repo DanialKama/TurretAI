f4b2ad766ec7ad03f9f7a986a70fbb43aac76af8
Danial Kama <dankam77.dk@gmail.com>
2023-01-12 11:02:35 +0330

    Implement projectile class
    Base class

---
+++ Source/CannonAI/Private/Actors/Projectile.cpp
@@ -0,0 +1,106 @@
+// Copyright 2023 Danial Kamali. All Rights Reserved.
+
+#include "Actors/Projectile.h"
+
+#include "GameFramework/ProjectileMovementComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "NiagaraComponent.h"
+#include "NiagaraFunctionLibrary.h"
+#include "Sound/SoundCue.h"
+
+AProjectile::AProjectile()
+{
+	PrimaryActorTick.bCanEverTick = false;
+	PrimaryActorTick.bStartWithTickEnabled = false;
+	SetCanBeDamaged(false);
+	InitialLifeSpan = 5.0f;
+
+	ProjectileMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Projectile Mesh"));
+	RootComponent = ProjectileMesh;
+	ProjectileMesh->SetLinearDamping(0.0f);
+	ProjectileMesh->SetGenerateOverlapEvents(false);
+	ProjectileMesh->CanCharacterStepUpOn = ECB_No;
+	ProjectileMesh->SetCollisionProfileName(FName("BlockAllDynamic"));
+	ProjectileMesh->SetNotifyRigidBodyCollision(true);	// Simulation Generates Hit Events
+	ProjectileMesh->SetCanEverAffectNavigation(false);
+
+	TrailParticle = CreateDefaultSubobject<UNiagaraComponent>(TEXT("Trail Particle"));
+	TrailParticle->SetupAttachment(ProjectileMesh);
+
+	ProjectileMovement = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("Projectile Movement"));
+	
+	// Initialize variables
+	bDoOnceHit = true;
+}
+
+void AProjectile::BeginPlay()
+{
+	Super::BeginPlay();
+
+	ProjectileMesh->IgnoreActorWhenMoving(GetOwner(), true);
+
+	ProjectileMesh->OnComponentHit.AddDynamic(this, &AProjectile::ProjectileHit);
+
+	if (HomingTarget)
+	{
+		ProjectileMovement->HomingTargetComponent = HomingTarget;
+		ProjectileMovement->ProjectileGravityScale = 0.0f;
+		ProjectileMovement->bIsHomingProjectile = true;
+	}
+}
+
+void AProjectile::ProjectileHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
+{
+	if (bDoOnceHit)
+	{
+		bDoOnceHit = false;
+		ProjectileMesh->SetNotifyRigidBodyCollision(false);
+		
+		// Is server
+		if (GetWorld()->GetNetMode() != NM_Client)
+		{
+			if (ProjectileAbility & Ability_Explosive)
+			{
+				ApplyExplosiveHit(Hit);
+			}
+			else
+			{
+				ApplyNormalHit(Hit);
+			}
+		}
+		
+		DisableProjectile();
+	}
+}
+
+void AProjectile::ApplyNormalHit(FHitResult HitResult) const
+{
+	UGameplayStatics::ApplyPointDamage(HitResult.GetActor(), DamageInfo.BaseDamage, HitResult.ImpactNormal, HitResult,
+		GetInstigatorController(), GetOwner(), nullptr);
+}
+
+void AProjectile::ApplyExplosiveHit(FHitResult HitResult) const
+{
+	const TArray<AActor*> Actors;
+	UGameplayStatics::ApplyRadialDamageWithFalloff(GetWorld(), DamageInfo.BaseDamage, DamageInfo.MinimumDamage, HitResult.ImpactPoint,
+		DamageInfo.InnerRadius, DamageInfo.OuterRadius, 1.0f, nullptr, Actors, GetOwner(), GetInstigatorController());
+}
+
+void AProjectile::DisableProjectile()
+{
+	FFXSystemSpawnParameters SpawnParams;
+	SpawnParams.WorldContextObject = GetWorld();
+	SpawnParams.SystemTemplate = HitParticle;
+	SpawnParams.Location = GetActorLocation();
+	UNiagaraFunctionLibrary::SpawnSystemAtLocationWithParams(SpawnParams);
+	
+	UGameplayStatics::SpawnSoundAtLocation(GetWorld(), HitSound, GetActorLocation());
+
+	ProjectileMesh->SetSimulatePhysics(false);
+	ProjectileMesh->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	ProjectileMesh->SetVisibility(false);
+		
+	TrailParticle->Deactivate();
+		
+	SetLifeSpan(2.0f);
+}
---
+++ Source/CannonAI/Public/Actors/Projectile.h
@@ -0,0 +1,69 @@
+// Copyright 2023 Danial Kamali. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DamageEvents.h"
+#include "GameFramework/Actor.h"
+#include "Projectile.generated.h"
+
+// TODO: Try to implement bit flags in a better way
+// Projectile Abilities
+constexpr uint8 Ability_None		{1 << 0}; // 0000 0001
+constexpr uint8 Ability_Explosive	{1 << 1}; // 0000 0010
+
+/**
+ * Base class for projectiles
+ */
+UCLASS()
+class CANNONAI_API AProjectile : public AActor
+{
+	GENERATED_BODY()
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = true))
+	TObjectPtr<UStaticMeshComponent> ProjectileMesh;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = true))
+	TObjectPtr<class UNiagaraComponent> TrailParticle;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = true))
+	TObjectPtr<class UProjectileMovementComponent> ProjectileMovement;
+
+// Functions
+public:
+	/** Sets default values for this actor's properties */
+	AProjectile();
+
+protected:
+	/** Called when the game starts or when spawned */
+	virtual void BeginPlay() override;
+
+private:
+	UFUNCTION()
+	void ProjectileHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
+
+	void ApplyNormalHit(FHitResult HitResult) const;
+	void ApplyExplosiveHit(FHitResult HitResult) const;
+
+	/** Disabling the projectile after hit and destroying it with a delay so trail particles have time to disappear */
+	void DisableProjectile();
+
+// Variables
+public:
+	TObjectPtr<USceneComponent> HomingTarget;
+
+	uint8 ProjectileAbility = Ability_None;
+
+private:
+	/** For non-explosive projectiles, only Base Damage is required */
+	UPROPERTY(EditDefaultsOnly, Category = "Default", meta = (AllowPrivateAccess = true))
+	FRadialDamageParams DamageInfo;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Default", meta = (AllowPrivateAccess = true))
+	TObjectPtr<class UNiagaraSystem> HitParticle;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Default", meta = (AllowPrivateAccess = true))
+	TObjectPtr<class USoundCue> HitSound;
+
+	uint8 bDoOnceHit : 1;
+};
