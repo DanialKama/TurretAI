e1d509dbfbca3dee1152ad801938f4f443497009
Danial Kama <dankam77.dk@gmail.com>
2023-04-25 20:24:08 +0330

    Create Destroyed Structure actor class

---
+++ Source/TurretAI/Private/Actors/DestroyedStructure.cpp
@@ -0,0 +1,58 @@
+﻿// Copyright 2023 Danial Kamali. All Rights Reserved.
+
+#include "DestroyedStructure.h"
+
+#include "Components/StaticMeshComponent.h"
+
+ADestroyedStructure::ADestroyedStructure()
+{
+	PrimaryActorTick.bCanEverTick = false;
+	PrimaryActorTick.bStartWithTickEnabled = false;
+	SetCanBeDamaged(false);
+	InitialLifeSpan = 6.0f;
+
+	StaticMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	RootComponent = StaticMesh;
+	StaticMesh->bApplyImpulseOnDamage = false;
+	StaticMesh->SetCollisionProfileName("Destructible");
+	StaticMesh->SetGenerateOverlapEvents(false);
+	StaticMesh->SetNotifyRigidBodyCollision(true);
+	StaticMesh->SetCanEverAffectNavigation(false);
+
+	// Initialize variables
+	bDoOnceHit = true;
+}
+
+void ADestroyedStructure::Initialize(UStaticMesh* InMesh, TArray<UMaterialInterface*> Materials, const float InLinearDamping, const float InAngularDamping) const
+{
+	StaticMesh->SetStaticMesh(InMesh);
+
+	for (int32 i = 0; i < Materials.Num(); ++i)
+	{
+		StaticMesh->SetMaterial(i, Materials[i]);
+	}
+
+	StaticMesh->SetLinearDamping(InLinearDamping);
+	StaticMesh->SetAngularDamping(InAngularDamping);
+
+	StaticMesh->SetSimulatePhysics(true);
+
+	StaticMesh->OnComponentHit.AddDynamic(this, &ADestroyedStructure::MeshHit);
+}
+
+void ADestroyedStructure::MeshHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
+{
+	if (bDoOnceHit)
+	{
+		bDoOnceHit = false;
+		StaticMesh->SetNotifyRigidBodyCollision(false);
+
+		FTimerHandle TimerHandle;
+		GetWorld()->GetTimerManager().SetTimer(TimerHandle, this, &ADestroyedStructure::StartSink, GetLifeSpan() - 2.0f);
+	}
+}
+
+void ADestroyedStructure::StartSink() const
+{
+	StaticMesh->SetCollisionResponseToChannel(ECC_WorldStatic, ECR_Ignore);
+}
---
+++ Source/TurretAI/Private/Actors/DestroyedStructure.h
@@ -0,0 +1,32 @@
+﻿// Copyright 2023 Danial Kamali. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "DestroyedStructure.generated.h"
+
+UCLASS()
+class TURRETAI_API ADestroyedStructure : public AActor
+{
+	GENERATED_BODY()
+
+	UPROPERTY()
+	TObjectPtr<UStaticMeshComponent> StaticMesh;
+
+// Functions
+public:
+	ADestroyedStructure();
+
+	void Initialize(UStaticMesh* InMesh, TArray<UMaterialInterface*> Materials, const float InLinearDamping, const float InAngularDamping) const;
+
+private:
+	UFUNCTION()
+	void MeshHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
+
+	void StartSink() const;
+
+// Variables
+private:
+	uint8 bDoOnceHit : 1;
+};
